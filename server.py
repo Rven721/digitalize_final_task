8cbf105329ea7553551c3701361d5f17e7c892cd
Rven721 <amalyavin1@gmail.com>
2022-04-26 08:15:20 +0300

    adding_phone_from_query_normalizer

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """Server for final task of DIGITALIZE"""
 from loguru import logger
-from fastapi import FastAPI, Body, Response, Form
+from fastapi import FastAPI, Body, Response, Form, Request
 from phone_standartizator import get_standart_phone
 
 logger.remove()
@@ -11,7 +11,7 @@ app = FastAPI()
 @app.get("/")
 def index_page():
     """Will handle first page"""
-    return Response("Well hello!", media_type="text/html")
+    return Response("Well helllo!", media_type="text/html")
 
 @app.post("/unify_phone_from_json")
 def noramlize_phone_from_json(data : dict = Body(...)):
@@ -26,6 +26,13 @@ def noramlize_phone_from_json(data : dict = Body(...)):
 def noramlize_phone_from_form(phone : str = Form(...)):
     """Will hendle phone request page"""
     logger.info(phone)
+    standart_phone = get_standart_phone(phone)
+    logger.info(standart_phone)
+    return Response(standart_phone, media_type="text/html")
+
+@app.get("/unify_phone_from_query")
+def normalize_phone_from_query(phone: str):
+    """Will return normalized fone goten from query"""
     standart_phone = get_standart_phone(phone)
     logger.info(standart_phone)
     return Response(standart_phone, media_type="text/html")
