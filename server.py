3953dbea5e20e1ffe10a6affff89bee4fda39b96
Rven721 <amalyavin1@gmail.com>
2022-04-26 08:44:42 +0300

    adding phone standartization from cookies

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """Server for final task of DIGITALIZE"""
 from loguru import logger
-from fastapi import FastAPI, Body, Response, Form, Request
+from fastapi import FastAPI, Body, Response, Form, Cookie
 from phone_standartizator import get_standart_phone
 
 logger.remove()
@@ -15,24 +15,33 @@ def index_page():
 
 @app.post("/unify_phone_from_json")
 def noramlize_phone_from_json(data : dict = Body(...)):
-    """Will hendle phone request page"""
+    """Will retrun normalized phone gotten form a body of request"""
     raw_phone = data['phone']
-    logger.info(raw_phone)
+    logger.info(f"Got {raw_phone}")
     standart_phone = get_standart_phone(raw_phone)
-    logger.info(standart_phone)
+    logger.info(f"Will return {standart_phone}")
     return Response(standart_phone, media_type="text/html")
 
 @app.post("/unify_phone_from_form")
 def noramlize_phone_from_form(phone : str = Form(...)):
-    """Will hendle phone request page"""
-    logger.info(phone)
+    """Will return normalized phone gotten from form"""
+    logger.info(f"Got {phone}")
     standart_phone = get_standart_phone(phone)
-    logger.info(standart_phone)
+    logger.info(f"Will return {standart_phone}")
     return Response(standart_phone, media_type="text/html")
 
 @app.get("/unify_phone_from_query")
 def normalize_phone_from_query(phone: str):
-    """Will return normalized fone goten from query"""
+    """Will return normalized phone gotten from query"""
+    logger.info(f"Got {phone}")
     standart_phone = get_standart_phone(phone)
-    logger.info(standart_phone)
+    logger.info(f"Will return {standart_phone}")
+    return Response(standart_phone, media_type="text/html")
+
+@app.get("/unify_phone_from_cookies")
+def normalize_phone_from_cookies(phone: str = Cookie(...)):
+    """Will return normalized phone gottern from cookies"""
+    logger.info(f"Got {phone}")
+    standart_phone = get_standart_phone(phone)
+    logger.info(f"Will return {standart_phone}")
     return Response(standart_phone, media_type="text/html")
